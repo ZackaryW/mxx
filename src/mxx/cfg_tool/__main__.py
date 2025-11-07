43e53da682a83c64a4bf74aaf289b4dcde312158
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-11-07 13:25:10 -0800

    fix: wrong import

--- src/mxx/cfg_tool/__main__.py
+++ src/mxx/cfg_tool/__main__.py
@@ -7,7 +7,7 @@ def main():
     """MXX Configuration Tool"""
     pass
 
-from mxx.cfg_tool import app # noqa
+from mxx.cfg_tool.app import app # noqa
 from mxx.cfg_tool.cfg import cfg # noqa
 main.add_command(app)
 main.add_command(cfg)
